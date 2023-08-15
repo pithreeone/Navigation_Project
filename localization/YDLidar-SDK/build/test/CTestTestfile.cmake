c79728bfd8115e5a049f87675827fa22b8b8c609
pithreeone <pithreeone@gmail.com>
2023-08-15 01:11:40 +0000

    Add: clone ydlidar_ros_driver and YDLidar-SDK

---
+++ localization/YDLidar-SDK/build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pithreeone/amr_robot/src/navigation/localization/YDLidar-SDK/test
+# Build directory: /home/pithreeone/amr_robot/src/navigation/localization/YDLidar-SDK/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(lidar_test "lidar_test")
+set_tests_properties(lidar_test PROPERTIES  _BACKTRACE_TRIPLES "/home/pithreeone/amr_robot/src/navigation/localization/YDLidar-SDK/test/CMakeLists.txt;10;add_test;/home/pithreeone/amr_robot/src/navigation/localization/YDLidar-SDK/test/CMakeLists.txt;0;")
