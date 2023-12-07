b75b9a54b68cb0bbeafe91c15a3b05fe7bd327ee
pithreeone <pithreeone@gmail.com>
2023-12-08 00:29:09 +0800

    Add: new launch file tracker_gazebo.launch

--- navigation/tracking_system/src/tracker.cpp
+++ navigation/tracking_system/src/tracker.cpp
@@ -3,9 +3,12 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/Twist.h>
 
+#define PI 3.1415926
+
 double target_x;
 double target_y;
 double target_yaw;
+double target_direction;  // 0: 0deg, 1: 90deg, 2: 180deg, 3: 270deg
 
 void targetCallback(const geometry_msgs::Pose::ConstPtr& msg)
 {
@@ -19,21 +22,34 @@ void targetCallback(const geometry_msgs::Pose::ConstPtr& msg)
     qt.getRPY(_, _, yaw);
     target_yaw = yaw;
     // ROS_INFO("I heard: [%s]", msg->data.c_str());
+
+    // ROS_INFO("atan2: %f", atan2(target_y, target_x));
+    target_direction = atan2(target_y, target_x) / (PI / 2);
+    // ROS_INFO("dir is: %f", dir);
+
+    ROS_INFO("Target Direction: %f", target_direction);
 }
 
 void trackingController(double& vx, double& vy, double& w){
     double dist = std::sqrt(target_x * target_x + target_y * target_y);
     // ROS_INFO("target_x: %f, target_y: %f", target_x, target_y);
-    if(dist <= 1.0){
+    double stop_distance = 0.5;
+    if(dist <= stop_distance){
         vx = 0;
         vy = 0;
         w = 0;
         return;
     }
-    double v = 0.2;
-    vx = 0.2 * target_x / sqrt(target_x * target_x + target_y * target_y);
-    vy = 0.2 * target_y / sqrt(target_x * target_x + target_y * target_y);
-    w = 0;
+    double maximum_velocity = 0.3;
+    double maximum_angular_velocity = 1;
+    vx = maximum_velocity * target_x / sqrt(target_x * target_x + target_y * target_y);
+    vy = maximum_velocity * target_y / sqrt(target_x * target_x + target_y * target_y);
+
+    if((0 < target_direction && target_direction <= 0.5) || (1 < target_direction && target_direction <= 1.5) || (-2 < target_direction && target_direction <= -1.5) || (-1 < target_direction && target_direction <= -0.5)){
+        w = maximum_velocity;
+    }else if((0.5 < target_direction && target_direction <= 1) || (1.5 < target_direction && target_direction <= 2) || (-2 < target_direction && target_direction <= -1.5) || (-1 < target_direction && target_direction <= -0.5)){
+        w = -maximum_velocity;
+    }
     
 }
 
